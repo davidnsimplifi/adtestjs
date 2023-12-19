45a03102214b27227733c7ace907574b161ebc71
David Nimon <davidn@simpli.fi>
2023-12-19 15:06:09 -0600

    Create ad3.js

---
+++ ad3.js
@@ -0,0 +1,154 @@
+(function(wnd, doc) {
+var sifi = {
+  adid:                   '33395920',
+  adposition:             'above',
+  adsize:                 '320x50',
+  advertiser:             'South+Bend+Orthopaedics',
+  appid:                  '6562021341218',
+  bidip:                  "24.15.28.70",
+  cachebust:              '1703017622061',
+  campaignid:             '3662917',
+  clicktag:               'https://eastads.simpli.fi/ctr?sifi=8753,3662917,33395920,0,28,0,0,0,108592268411501,235,40,v,12.21908,${AUCTION_PRICE},0,0,1,12,3C6E0C91E23592BB5B9CC663A9D6AC33,2,0,0,1270,400,1,0,0,604,0,702,bidder-base-prd-us-east4-b-4c764af7:9041-1703010615064-493732339,1,0,395291,2,0,30,12,46530,0,0.00,1,6562021341218,403643462,0,0,0,1,-5,0,33,,0,446282,0,0,2,25,adc19250b7eb17fe59473bf96227c3d6,2,-1,4920869&tid=f0d2880bcca0ddcc1f3dd07ae9b8daa1983cc3d7&turl=',
+  clienttrackingid:       '96CBCFCD70C81BCA0DBB0C1DE9BDE2CE',
+  contextid:              '235',
+  countryid:              '8180',
+  darasid:                'asid,P21DF74DF-9CD9-4B57-BC30-13908353E1B3',
+  dardevid:               'devid,96CBCFCD70C81BCA0DBB0C1DE9BDE2CE',
+  dardevgrp:              'devgrp,PHN', 
+  darosgrp:               'osgrp,ANDROID',
+  darplatform:            'plt,MBL',
+  daruoo:                 'false',
+  datapartnerid:          '0',
+  docbase:                'https://media.simpli.fi/ads/display/10/33395920/d9c6760e3ef306ed058dfddd9ea7644c/',
+  domainid:               '0',
+  doubleescapedclicktag:  'https%253A%252F%252Feastads.simpli.fi%252Fctr%253Fsifi%253D8753%252C3662917%252C33395920%252C0%252C28%252C0%252C0%252C0%252C108592268411501%252C235%252C40%252Cv%252C12.21908%252C%2524%257BAUCTION_PRICE%257D%252C0%252C0%252C1%252C12%252C3C6E0C91E23592BB5B9CC663A9D6AC33%252C2%252C0%252C0%252C1270%252C400%252C1%252C0%252C0%252C604%252C0%252C702%252Cbidder-base-prd-us-east4-b-4c764af7%253A9041-1703010615064-493732339%252C1%252C0%252C395291%252C2%252C0%252C30%252C12%252C46530%252C0%252C0.00%252C1%252C6562021341218%252C403643462%252C0%252C0%252C0%252C1%252C-5%252C0%252C33%252C%252C0%252C446282%252C0%252C0%252C2%252C25%252Cadc19250b7eb17fe59473bf96227c3d6%252C2%252C-1%252C4920869%2526tid%253Df0d2880bcca0ddcc1f3dd07ae9b8daa1983cc3d7%2526turl%253D',
+  escapedclicktag:        'https%3A%2F%2Feastads.simpli.fi%2Fctr%3Fsifi%3D8753%2C3662917%2C33395920%2C0%2C28%2C0%2C0%2C0%2C108592268411501%2C235%2C40%2Cv%2C12.21908%2C%24%7BAUCTION_PRICE%7D%2C0%2C0%2C1%2C12%2C3C6E0C91E23592BB5B9CC663A9D6AC33%2C2%2C0%2C0%2C1270%2C400%2C1%2C0%2C0%2C604%2C0%2C702%2Cbidder-base-prd-us-east4-b-4c764af7%3A9041-1703010615064-493732339%2C1%2C0%2C395291%2C2%2C0%2C30%2C12%2C46530%2C0%2C0.00%2C1%2C6562021341218%2C403643462%2C0%2C0%2C0%2C1%2C-5%2C0%2C33%2C%2C0%2C446282%2C0%2C0%2C2%2C25%2Cadc19250b7eb17fe59473bf96227c3d6%2C2%2C-1%2C4920869%26tid%3Df0d2880bcca0ddcc1f3dd07ae9b8daa1983cc3d7%26turl%3D',
+  escapedkeyword:         'health_care_services_optimized_audience',
+  escapedreferer:         '',
+  escapedtrackingpixel:   'https%3A%2F%2Feastads.simpli.fi%2Fctr%3Fsifi%3D8753%2C3662917%2C33395920%2C0%2C28%2C0%2C0%2C0%2C108592268411501%2C235%2C40%2Cv%2C12.21908%2C%24%7BAUCTION_PRICE%7D%2C0%2C0%2C1%2C12%2C3C6E0C91E23592BB5B9CC663A9D6AC33%2C2%2C0%2C0%2C1270%2C400%2C1%2C0%2C0%2C604%2C0%2C702%2Cbidder-base-prd-us-east4-b-4c764af7%3A9041-1703010615064-493732339%2C1%2C0%2C395291%2C2%2C0%2C30%2C12%2C46530%2C0%2C0.00%2C1%2C6562021341218%2C403643462%2C0%2C0%2C0%2C1%2C-5%2C0%2C33%2C%2C0%2C446282%2C0%2C0%2C2%2C25%2Cadc19250b7eb17fe59473bf96227c3d6%2C2%2C-1%2C4920869%26tid%3Df0d2880bcca0ddcc1f3dd07ae9b8daa1983cc3d7%26turl%3D',
+  exchange_id:            '40',
+  exchange_uid:           'fc3c76e5-b77b-47e9-b633-2ae60aef30cc',
+  exchangeid:             '40',
+  exchangeuserid:         'fc3c76e5-b77b-47e9-b633-2ae60aef30cc',
+  geofenceid:             '0',
+  geoid:                  '4920869',
+  height:                 '50',
+  ip:                     '70.118.118.14',
+  keyword:                'health_care_services_optimized_audience',
+  keywordid:              '108592268411501',
+  metroid:                '15138',
+  postalcode:             '46530',
+  queryparams:            'sifi=8753,3662917,33395920,0,28,0,0,0,108592268411501,235,40,v,12.21908,${AUCTION_PRICE},0,0,1,12,3C6E0C91E23592BB5B9CC663A9D6AC33,2,0,0,1270,400,1,0,0,604,0,702,bidder-base-prd-us-east4-b-4c764af7:9041-1703010615064-493732339,1,0,395291,2,0,30,12,46530,0,0.00,1,6562021341218,403643462,0,0,0,1,-5,0,33,,0,446282,0,0,2,25,adc19250b7eb17fe59473bf96227c3d6,2,-1,4920869&hash_rid=96CBCFCD70C81BCA0DBB0C1DE9BDE2CE&sifi_exchange_uid=fc3c76e5-b77b-47e9-b633-2ae60aef30cc&request_id=f0d2880bcca0ddcc1f3dd07ae9b8daa1983cc3d7&sifi_kw=health_care_services_optimized_audience',
+  referer:                '',
+  regionid:               '8202',
+  sifi_uid:               '3C6E0C91E23592BB5B9CC663A9D6AC33',
+  sifiday:                '8753',
+  sifiimpression:         'http://eastads.simpli.fi/ads/3662917/33395920/ad.html?sifi=8753,3662917,33395920,0,28,0,0,0,108592268411501,235,40,v,12.21908,${AUCTION_PRICE},0,0,1,12,3C6E0C91E23592BB5B9CC663A9D6AC33,2,0,0,1270,400,1,0,0,604,0,702,bidder-base-prd-us-east4-b-4c764af7:9041-1703010615064-493732339,1,0,395291,2,0,30,12,46530,0,0.00,1,6562021341218,403643462,0,0,0,1,-5,0,33,,0,446282,0,0,2,25,adc19250b7eb17fe59473bf96227c3d6,2,-1,4920869&hash_rid=96CBCFCD70C81BCA0DBB0C1DE9BDE2CE&sifi_exchange_uid=fc3c76e5-b77b-47e9-b633-2ae60aef30cc&request_id=f0d2880bcca0ddcc1f3dd07ae9b8daa1983cc3d7&sifi_kw=health_care_services_optimized_audience',
+  sifitest:               '',
+  sifiuserid:             '3C6E0C91E23592BB5B9CC663A9D6AC33',
+  sifivideotrackingurl:   'https://events.simpli.fi/ve',
+  siteid:                 '6562021341218',
+  targeturl:              'https://www.sbortho.com/?utm_source=cvrdisplay&utm_medium=display&utm_campaign=comp-services&utm_term=&utm_content=opt-1',
+  trackingpixel:          'https://eastads.simpli.fi/ctr?sifi=8753,3662917,33395920,0,28,0,0,0,108592268411501,235,40,v,12.21908,${AUCTION_PRICE},0,0,1,12,3C6E0C91E23592BB5B9CC663A9D6AC33,2,0,0,1270,400,1,0,0,604,0,702,bidder-base-prd-us-east4-b-4c764af7:9041-1703010615064-493732339,1,0,395291,2,0,30,12,46530,0,0.00,1,6562021341218,403643462,0,0,0,1,-5,0,33,,0,446282,0,0,2,25,adc19250b7eb17fe59473bf96227c3d6,2,-1,4920869&tid=f0d2880bcca0ddcc1f3dd07ae9b8daa1983cc3d7&turl=',
+  transactionid:          'bidder-base-prd-us-east4-b-4c764af7:9041-1703010615064-493732339',
+  trustesid:              '7BFq-tseoZb_N8y1CeH8CJ6lD4BRzEOb5nYWHrpoKi0ghJk1yGLRXc7uF8yXWhKltV3h-CuOmakju9_4u6HpVkCi1PjJ1zymX9n-1LmtTqunXi2HVegW0uQ4tMPldpLxGJpevIMQESUV47uef4dLWVSrH0PLe_O3uISzpgRKIeQ',
+  useragent:              'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+  uuid:                   '3a4583d1f8c912864617dda85b1dad042ad9ebc9',
+  videoadtype:            '',
+  width:                  '320'
+};
+
+sifi.clickmacro = sifi.clicktag + wnd.encodeURIComponent(sifi.targeturl);
+
+function fetchAdMarkup(url, callback) {
+  var xmlhttp;
+  var ie = wnd.XDomainRequest;
+
+  if (ie) {
+    xmlhttp = new wnd.XDomainRequest();
+    xmlhttp.onprogress = function() {};
+    xmlhttp.ontimeout = function() {};
+    xmlhttp.onerror = function() {};
+    xmlhttp.onload = function() {
+      callback(xmlhttp.responseText);
+    };
+  } else if (wnd.XMLHttpRequest) { // modern browsers
+    xmlhttp = new wnd.XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == wnd.XMLHttpRequest.DONE) {
+        if (xmlhttp.status < 400) {
+          callback(xmlhttp.responseText);
+        }
+      }
+    };
+  }
+
+  console.log('url 2 - hello?', url);
+  if(location.protocol === 'file:') {
+    console.log('file mode');
+    xmlhttp.open('GET', url, true);
+  } else {
+    console.log('non-file mode');
+    xmlhttp.open('GET', url.replace(/http(s)?:\/\//, '//'), true);
+  }
+  
+  xmlhttp.send();
+}
+
+function render(url, macros) {
+  fetchAdMarkup(url, function(html) {
+    console.log('html response');
+    renderTarget(replaceMacros(html, macros));
+  });
+}
+
+function replaceMacros(html, macros) {
+  return html.replace(/\{\{(\S+?)\}\}/gi, function swap(match, $1) {
+    var macroValue = macros[$1.toLowerCase()]
+    return macroValue === undefined ? $1 : macroValue;
+  });
+}
+
+function heredoc() {/*
+
+
+
+
+  <span id="te-clearads-js-cont1">
+    <script src="https://choices.truste.com/ca?pid=simplifi01&aid=simplifi01&cid=0611sf320x50&c=cont1&w=320&h=50&js=pmw0&plc=tr&sid=7BFq-tseoZb_N8y1CeH8CJ6lD4BRzEOb5nYWHrpoKi0ghJk1yGLRXc7uF8yXWhKltV3h-CuOmakju9_4u6HpVkCi1PjJ1zymX9n-1LmtTqunXi2HVegW0uQ4tMPldpLxGJpevIMQESUV47uef4dLWVSrH0PLe_O3uISzpgRKIeQ"></script>
+  </span>
+
+
+  <script src="https://cdn.simpli.fi/lib/SifiLib-1.1.12.min.js"
+          id="viewpx"
+          app="6562021341218"
+          publisher="6562021341218"
+          ad_id="33395920"
+          sifi_day="8753"
+          sifi_uid="3C6E0C91E23592BB5B9CC663A9D6AC33"
+          tid="bidder-base-prd-us-east4-b-4c764af7:9041-1703010615064-493732339"
+          cid="3662917"
+          device="702"
+          heartbeat="false"
+          width="320"
+          height="50"></script>
+
+*/}
+
+function transform(func) {
+  // converts a heredoc-formatted function to a string that represents its content
+  return func.toString().split('\n').slice(1, -1).join('\n');
+}
+
+  function renderTarget(html) {
+    console.log('renderTarget');
+    doc.open();
+    console.log('renderTarget 2');
+    doc.write(html.replace(/<\/body>/gi, transform(heredoc) + '\n</body>'));
+    console.log('renderTarget 3');
+    doc.close();
+    console.log('renderTarget 4');
+  }
+
+  render('https://media.simpli.fi/ads/display/10/33395920/d9c6760e3ef306ed058dfddd9ea7644c/index.html', sifi);
+})(window, document);
